4735c738fc368249d77ea2ad57d16a868843cb49
somozadev <somozadev@gmail.com>
2020-11-09 13:05:07 +0100

    pc working

--- Assets/Graphic/CubeGraph.cs
+++ Assets/Graphic/CubeGraph.cs
@@ -7,49 +7,56 @@ public class CubeGraph : MonoBehaviour
 {
     [SerializeField]    GameObject prefab;
     [Range(10,100)]public int resolution = 10;
+    [SerializeField] FunctionLibrary.FunctionNames function = default;
     Transform[] points;
     float step, time;
+    FunctionLibrary.Function f;
     Vector3 position;
     void Awake()
     {
         position = Vector3.zero;
+        f = FunctionLibrary.GetFunction(function);
+
+        step = 2f / resolution;
+        points = new Transform[resolution * resolution];
         CreateGraph();
     }
 
     void DrawGraph()
     {
-           for(int i = 0; i < points.Length; i++)
+            for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
             {
-                Transform point = points[i];
-                Vector3 position = point.localPosition;
-            position.y = FunctionLibrary.Wave(position.x,time);
-                point.localPosition = position;
+                if (x == resolution) { x = 0; z += 1; }
+                float u = (x + 0.5f) * step - 1f;
+                float v = (z + 0.5f) * step - 1f;
+                points[i].localPosition = f(u, v, time);
             }
         
     } 
     void CreateGraph()
     {
 
-        step = 2f/resolution;
-        var position = Vector3.zero;
-        
-        points = new Transform[resolution];
 
-        for(int i = 0; i < points.Length; i++)
+        for(int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
+            if (x == resolution) { x = 0; z += 1; }
             Transform p = Instantiate(prefab).transform;
-            p.SetParent(transform,false);
-            position.x = (i + 0.5f) * step - 1f;
-            position.y = FunctionLibrary.MultiWave(position.x,time);
+            position.x = (x + 0.5f) * step - 1f;
+            position.z = (z + 0.5f) * step - 1f;
             p.localPosition = position;
+            p.SetParent(transform, false);
             points[i] = p;
             p.localScale = Vector3.one * step;
 
         }
     }
 
+    
+
     void Update()
     {
+
+        f = FunctionLibrary.GetFunction(function);
         time = Time.time;
         DrawGraph();
     }
--- Assets/Mathematical Surfaces/FunctionLibrary.cs
+++ Assets/Mathematical Surfaces/FunctionLibrary.cs
@@ -5,22 +5,65 @@ using UnityEngine;
 
 public static class FunctionLibrary 
 {
-    public static float Wave(float x, float t)
+
+    public delegate Vector3 Function(float u, float v, float t);
+    public enum FunctionNames { Wave, MultiWave, Logaritmic, Ripple, Sphere };
+
+    static Function[] functions = { Wave, MultiWave, Logaritmic, Ripple, Sphere};
+    public static Function GetFunction (FunctionNames name) { return functions[(int)name]; }
+
+
+
+
+
+    public static Vector3 Wave(float u, float v, float t)
     {
-        return Mathf.Sin(Mathf.PI * (x + t));
+        Vector3 p;
+        p.x = u;
+        p.y = Mathf.Sin(Mathf.PI * (u + v + t));
+        p.z = v;
+        return p;
     } 
     
-    public static float MultiWave(float x, float t)
+    public static Vector3 MultiWave(float u, float v, float t)
     {
-        float y = Mathf.Sin(Mathf.PI * (x + t));
-        y+= Mathf.Sin(2f * Mathf.PI * (x+t))/2;
-        return y/1.5f; 
-    } 
-    public static float MultiWave2(float x, float t)
+        Vector3 p;
+        p.x = u;
+        p.y = Mathf.Sin(Mathf.PI * (u + 0.5f * t));
+        p.y += 0.5f * Mathf.Sin(2f * Mathf.PI * (v + t)) / 2; 
+        p.y += Mathf.Sin(Mathf.PI * (u + v + 0.25f * t));
+        p.y *= 1f / 2.5f;
+        p.z = v;
+        
+        return p;
+    }
+    public static Vector3 Logaritmic(float u, float v, float t)
     {
-        float y = Mathf.Sin(Mathf.PI * (x + t));
-        y += Mathf.Sin(2f * Mathf.PI * (x + t)) * (1f / 2f);
-		return y * (2f / 3f);
+        Vector3 p;
+        p.x = Mathf.Log(u + 2);
+        p.y = Mathf.Tan(Mathf.PI * (v + t / 2));
+        p.z = v;
+        return p;
+    }
+    public static Vector3 Ripple(float u, float v, float t)
+    {
+        Vector3 p;
+        float d = Mathf.Sqrt(u * u + v * v);
+        p.x = u;
+        p.y = Mathf.Sin(Mathf.PI * (4f * d - t));
+        p.y /= 1f + 10f * d;
+        p.z = v;
+        return p;
+    }
+    public static Vector3 Sphere(float u, float v, float t)
+    {
+        Vector3 p;
+        float r = Mathf.Cos(0.5f * Mathf.PI * v);
+
+        p.x = r * Mathf.Sin(Mathf.PI * u);
+        p.y = Mathf.Sin(Mathf.PI * 0.5f * v);
+        p.z = r * Mathf.Sin(Mathf.PI * u);
+        return p;
     }
 
 
